6be9846e83745afded33ba9a5c2cf356ba97b563
Cynosure159 <15217310106@163.com>
2026-09-01 14:31:46 +0800

    chore: remove obsolete internal/kodi package after full migration to internal/nfo

--- internal/kodi/movie.go
+++
@@ -1,9 +0,0 @@
-package kodi
-
-import "github.com/mediagrap/mediagrap/internal/nfo"
-
-type Movie = nfo.Movie
-type Person = nfo.Person
-
-var ParseMovie = nfo.ParseMovie
-var WriteMovie = nfo.WriteMovie
